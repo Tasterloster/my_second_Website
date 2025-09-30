a979fb893541a418e4e5e3ce456a21d013ba562f
lgeldmacher <l.geldmacher@x-cell.com>
2025-09-30 12:27:09 +0200

    changed fetchTodosFromPublic function to include parseTodos function

--- src/TodosIO.ts
+++ src/TodosIO.ts
@@ -34,7 +34,6 @@ export function downloadJSON(data: unknown, filename: string){
 }
 
 export async function fetchTodosFromPublic(filename: string = "todos.json"): Promise<Todo[]>{
-
     const base = new URL(import.meta.env.BASE_URL, window.location.origin)
     const url = new URL(filename, base).toString()
     console.log("[import] URL:", url)
@@ -47,16 +46,7 @@ export async function fetchTodosFromPublic(filename: string = "todos.json"): Pro
 
     const data = await res.json()
 
-    if(!Array.isArray(data)) {
-        throw new Error(`Invalid JSON: not an array`)
-    }
-
-    return data.map((t: any) => ({
-        id: Number(t.id),
-        text: String(t.text ?? ""),
-        deleted: Boolean(t.deleted),
-        checked: Boolean(t.checked)
-    }))
+    return parseTodos(data)
 }
 
 export async function createBlobForDownload(todos: Todo[]){
